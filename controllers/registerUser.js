c7d20c22136880f03e119335bf4038a9c3f44674
Alok Chauhan <chauhan.alok1995@gmail.com>
2023-01-15 19:28:07 +0530

    done

--- controllers/registerUser.js
+++ controllers/registerUser.js
@@ -1,14 +1,16 @@
 const UserModel =require("../models/userSchema")
-
-function RegisterUser(req, res) {
+const encrpty =require("../utils/encrpty")
+async function RegisterUser(req, res) {
     let username = req.body.username;
     let email = req.body.email;
     let password = req.body.password;
+    let hash = await encrpty(password);
     let userdata = {
         username: username,
         email: email,
-        password:password
+        password:hash
     }
+    //encrpty(password);
     let resultData = new UserModel(userdata);
     resultData.save().then(item => {
         res.send("user added successfully")
@@ -16,7 +18,5 @@ function RegisterUser(req, res) {
         console.log("error in adding data");
     })
 }
-
-
 
 module.exports = RegisterUser;
